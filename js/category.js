02316a2da5e7350fbd87e79bd66def82a544891c
zhaxxxybek <118388455+zhaxxxybek@users.noreply.github.com>
2022-11-17 09:59:29 +0600

    Update category.js

--- js/category.js
+++ js/category.js
@@ -9,3 +9,10 @@ MenuBtn.onclick = ()=>{
     }
 }
     
+    let query = document.querySelector('.query');
+    let searchBtn = document.querySelector('.searchBtn')
+
+    searchBtn.onclick = function() {
+        let url = 'https://www.google.com/search?q='+query.value;
+        window.open(url, '_self');
+    }
